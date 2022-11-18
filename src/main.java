6fe02519827f7e054b13a69f35e8564bb8ce6a6d
AdrianMacia <adrianmaciad123@gmail.com>
2022-11-18 19:22:29 +0100

    Primer commit

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Estudiante " + args[0] + " : ");
+
+
+        System.out.println(Veredicto(args[1],args[2]));
+
+
+    }
+
+    public static String Veredicto(String valor1, String valor2){
+
+        if (valor1.equals(valor2)){
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar mas tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+
+
+}
